895b2d0ee11349656035ae41404a6a576250b4d2
Anuska <majumderanuska121@gamil.com>
2024-03-14 22:25:41 +0530

    frist practice problen

---
+++ Practice/PCL01_problems_START01.c
@@ -0,0 +1,24 @@
+/*Number Mirror
+Write a program that takes a number 
+N as the input, and prints it to the output.
+
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line.*/
+#include <stdio.h>
+
+int main(void) {
+	int N;
+    
+    // Taking input from the user
+    
+    scanf("%d", &N);
+    
+    // Printing the number
+    printf("%d\n", N);
+    
+    return 0;
+
+}
