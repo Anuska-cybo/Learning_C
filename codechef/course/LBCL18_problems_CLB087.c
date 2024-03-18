fc3b40929fe8315e11801e64d2212d3d666a323f
Anuska <majumderanuska121@gamil.com>
2024-03-18 22:40:25 +0530

    code chef

---
+++ codechef/course/LBCL18_problems_CLB087.c
@@ -0,0 +1,15 @@
+/*Task
+Write a program which does the following
+
+Output the 3rd element from the given array to the console.*/
+#include <stdio.h>
+
+int main() {
+    
+    int num[ 5 ] = {1, 2, 3, 4, 5 };
+    // your code goes here
+    printf("%d", num[ 2 ]);
+
+    return 0;
+
+}
