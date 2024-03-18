fc86f47931835442aab6b89307530f47c49a9cc7
Anuska <majumderanuska121@gamil.com>
2024-03-18 22:49:04 +0530

    code chef

---
+++ codechef/course/LBCL18_problems_CLB089.c
@@ -0,0 +1,16 @@
+/*Displaying elements
+Write a program which does the following
+
+Create a string array for the following values "Monday", "Tuesday", "Wednesday", and "Thursday"
+Output the last two elements of the array on separate lines*/
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	char week[4][10] = {"Monday", "Tuesday", "Wednesday", "Thursday"};
+    printf("%s\n", week[ 2 ]);
+    printf("%s", week[ 3 ]);
+
+    return 0;
+
+}
