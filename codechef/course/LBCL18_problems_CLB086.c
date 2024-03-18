4fec78a29b7bd4b292f770cf25a2349ee0f4fbb6
Anuska <majumderanuska121@gamil.com>
2024-03-18 22:19:37 +0530

    code chef

---
+++ codechef/course/LBCL18_problems_CLB086.c
@@ -0,0 +1,15 @@
+/*Task
+Write a program which does the following
+
+Create an Array of the first 5 positive integers
+Once the array is define - output "Done" to the console.*/
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int num[ 5 ] = {1, 2, 3, 4, 5};
+    printf("Done");
+
+    return 0;
+
+}
